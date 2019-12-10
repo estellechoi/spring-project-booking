a6dbedb710382015065ef6ba128ebf3b038b2a82
Estelle Choi <estele.choi@gmail.com>
2019-12-10 17:01:39 +0900

    MVC 설정파일 : @ EnableWebMvc + WebMvcConfigurerAdapter 메소드 오버라이드

--- booking/src/main/java/com/youjin/booking/config/ContextConfig.java
+++ booking/src/main/java/com/youjin/booking/config/ContextConfig.java
@@ -1,5 +1,48 @@
 package com.youjin.booking.config;
 
-public class ContextConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc // MVC 모델의 기본 핸들러들을 생성하고 기본 설정해준다.
+@ComponentScan(basePackages = { "com.youjin.booking.controller" })
+public class ContextConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+		// default servlet handler를 사용하게 합니다.
+		// 매핑 정보가 없는 url 요청을 처리한다.
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(31556926);
+		registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(31556926);
+		registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(31556926);
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		System.out.println("addViewControllers가 호출합니다.");
+		registry.addViewController("/").setViewName("index");
+		// 특정 url 매핑을 여기에서 별도로 한다.
+	}
+
+	// view resolver 등록
+	@Bean
+	public InternalResourceViewResolver getInternalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
 
 }
